5044117d38a8b300c01bec6ea9f76ce9257c19f0
Jagi <ba87688@gmail.com>
2022-07-10 04:15:42 -0500

    Databinding added and a model class.

--- app/src/main/java/com/example/shoestore/MainActivity.kt
+++ app/src/main/java/com/example/shoestore/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
 
     }
---
+++ app/src/main/java/com/example/shoestore/models/Shoe.kt
@@ -0,0 +1,33 @@
+package com.example.shoestore.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Shoe(var name: String, var size:Double, var company:String,var description:String,
+var images: List<String> = mutableListOf()):Parcelable {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
